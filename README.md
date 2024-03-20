6c0568c2b71de373dc681a228b8bc10fbcf207d4
Tobias Liegl <tobiasliegl05@gmail.com>
2024-03-20 12:10:04 +0100

    2024_03_20

--- README.md
+++ README.md
@@ -2,7 +2,9 @@ Das ist das GnuRadio Prejekt von Ludwig Fattinger, Tobias Liegl und Stefan Strab
 
 # Protokoll
 
-## W1 | 6.3.2022
+__Thema: Gegensttion für PMR Funkgerät__
+
+## W1 | 6.3.2024
 
 * Driver installiert:
 
@@ -11,4 +13,22 @@ apt install libuhd-dev uhd-host
 uhd_images_downloader
 ```
 
-* Antenne funktioniert!
+* Antenne funktioniert! 
+
+## W2 | 20.03.2024
+__UKW Radio__
+- FM Radio erstellt mit FM-Decode. Wichtig ist
+die Werte des Rational Resamplers so umzustellen, damit die Soundkarte am Ende 
+48 kHz erhält. 
+- Test mit Ö3 und Kronehit erfolgreich
+- Ein FM Demod ist notwendig
+- Zwei HW Komponenten involviert, RF-Frontend (Antenne und SDR) und Soundkarte des Rechners  
+https://support.pervices.com/tutorials/pvt-1/  
+https://senderkataster.at/
+----
+__PMR Projekt__
+- Projektstart PMR (WalkieTalkie)
+- Abänderung des UKW Radios auf PMR-Frequenzen
+- Rund um 446 MHz, Kanalbreite 12.5 kHz, wichtig ist, dass beachtet wird, auf welchem Kanal gerade gesendet wird. 
+- NBFM (Narrow Band Frequency Modulation)  
+https://www.mwf-service.com/shop/pmr-funk.html
--- spektrumanalyzer.py
+++ spektrumanalyzer.py
@@ -24,6 +24,9 @@ from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -76,14 +79,15 @@ class spektrumanalyzer(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 20000000
-        self.center = center = 95000000
+        self.samp_rate = samp_rate = 10000000
+        self.center = center = 446081250
+        self.bandwidth = bandwidth = 576000
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._center_range = Range(80000000, 110000000, 100000, 95000000, 200)
+        self._center_range = Range(446006250, 446193750, 12500, 446081250, 200)
         self._center_win = RangeWidget(self._center_range, self.set_center, "'center'", "counter_slider", int, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._center_win)
         self.uhd_usrp_source_0 = uhd.usrp_source(
@@ -99,48 +103,19 @@ class spektrumanalyzer(gr.top_block, Qt.QWidget):
 
         self.uhd_usrp_source_0.set_center_freq(center, 0)
         self.uhd_usrp_source_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_source_0.set_bandwidth(bandwidth, 0)
         self.uhd_usrp_source_0.set_rx_agc(True, 0)
-        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            center, #fc
-            samp_rate, #bw
-            "Waterfall", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
-        self.qtgui_waterfall_sink_x_0.enable_grid(False)
-        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
-
-
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        colors = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
-            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
-
-        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
-
-        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=48,
+                decimation=10000,
+                taps=[],
+                fractional_bw=0)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             center, #fc
-            samp_rate, #bw
-            "Frequency", #name
+            bandwidth, #bw
+            "", #name
             1,
             None # parent
         )
@@ -149,7 +124,7 @@ class spektrumanalyzer(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
         self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
         self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(True)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
         self.qtgui_freq_sink_x_0.set_fft_average(1.0)
         self.qtgui_freq_sink_x_0.enable_axis_labels(True)
         self.qtgui_freq_sink_x_0.enable_control_panel(True)
@@ -177,13 +152,22 @@ class spektrumanalyzer(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_nbfm_rx_0 = analog.nbfm_rx(
+        	audio_rate=48000,
+        	quad_rate=192000,
+        	tau=(75e-6),
+        	max_dev=5e3,
+          )
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_nbfm_rx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_nbfm_rx_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.rational_resampler_xxx_0, 0))
 
 
     def closeEvent(self, event):
@@ -199,8 +183,6 @@ class spektrumanalyzer(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.center, self.samp_rate)
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.center, self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
     def get_center(self):
@@ -208,9 +190,16 @@ class spektrumanalyzer(gr.top_block, Qt.QWidget):
 
     def set_center(self, center):
         self.center = center
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.center, self.samp_rate)
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.center, self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.center, self.bandwidth)
         self.uhd_usrp_source_0.set_center_freq(self.center, 0)
+
+    def get_bandwidth(self):
+        return self.bandwidth
+
+    def set_bandwidth(self, bandwidth):
+        self.bandwidth = bandwidth
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.center, self.bandwidth)
+        self.uhd_usrp_source_0.set_bandwidth(self.bandwidth, 0)
 
 
 
